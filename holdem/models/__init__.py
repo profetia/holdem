13b09e27c607d1c12b563406c19c75bae525ff46
YaeSakuraL <yaesakura@qq.com>
2023-05-21 15:54:43 +0800

    Add: rule based agent

--- holdem/models/__init__.py
+++ holdem/models/__init__.py
@@ -1,3 +1,4 @@
 from .agent import Agent
 from .random import RandomAgent
 from .dqn import DQNAgent
+from .rule_based import RuleBasedAgent
---
+++ holdem/models/rule_based.py
@@ -0,0 +1,25 @@
+import numpy as np
+from .agent import Agent
+
+
+class RuleBasedAgent(Agent):
+    def __init__(self, num_actions):
+        self.num_actions = num_actions
+
+    def step(self, state):
+        raise NotImplementedError
+
+    def eval_step(self, state):
+        probs = [0 for _ in range(self.num_actions)]
+        for i in state["legal_actions"]:
+            probs[i] = 1 / len(state["legal_actions"])
+
+        info = {}
+        info["probs"] = {
+            state["raw_legal_actions"][i]: probs[
+                list(state["legal_actions"].keys())[i]
+            ]
+            for i in range(len(state["legal_actions"]))
+        }
+
+        return self.step(state), info
